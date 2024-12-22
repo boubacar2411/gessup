5c08e2b4f2551bc6b98c556b3075742c24cd4fc5
boubacar2411 <81101759+boubacar2411@users.noreply.github.com>
2024-12-22 12:31:58 +0000

    TEST

---
+++ src/main/java/sn/esp/pratique/PratiqueApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.pratique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PratiqueApplication.class, args);
+    }
+
+}
